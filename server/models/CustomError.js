e492e390bebf9c73b550e570bd9e5b2bf782566a
Nilesh101212 <142312149+Nilesh101212@users.noreply.github.com>
2024-06-23 00:20:25 +0530

    Delete server directory

--- server/models/CustomError.js
+++
@@ -1,9 +0,0 @@
-class CustomError extends Error {
-  constructor(message, errorCode, errorArray) {
-    super(message);
-    this.code = errorCode;
-    this.array = errorArray;
-  }
-}
-
-export default CustomError;
